a3bb1a7492b50c04d5bea2a76dc1142d8d262581
Dhianahdi <mongi.nahdi@gmail.com>
2024-03-20 02:36:07 +0100

    3rd

--- routes/medicalRecord.js
+++ routes/medicalRecord.js
@@ -3,5 +3,14 @@ const router = express.Router();
 const medicalRecordController = require('../controllers/medicalRecordController');
 
 router.get('/:patientId', medicalRecordController.getMedicalRecordByPatientId);
+router.post('/', medicalRecordController.createMedicalRecord);
 
+router.get('/', medicalRecordController.getAllMedicalRecords);
+
+
+router.put('/:patientId', medicalRecordController.updateMedicalRecord);
+
+router.delete('/:patientId', medicalRecordController.deleteMedicalRecord);
+
+module.exports = router;
 module.exports = router;
