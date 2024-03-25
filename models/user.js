764b83ac64e002167c9a8e9ce5c2dd61e02ac6c4
Louai-Souei <Louai.souei04@gmail.com>
2024-03-25 16:32:13 +0100

    userSchema Update

--- models/user.js
+++ models/user.js
@@ -1,16 +1,37 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new mongoose.Schema({
-  // nom: {type: String, required: true },
-  // prenom: { type: String, required: true },
-  // sexe: { type: String, enum: ["Male", "Female"] },
-  // date_naissance: { type: Date },
-  // tel: { type: String },
+  nom: {type: String, required: true },
+  prenom: { type: String, required: true },
+  sexe: { type: String, enum: ["Male", "Female"] },
+  date_naissance: { type: Date },
+  telephone: { type: String },
+  telephone_2: { type: String },
+  telephone_3: { type: String },
+  gouvernorat: {
+    type: String,
+    required: function () {
+      return this.role === "doctor";
+    },
+  },
+  avenue: {
+    type: String,
+    required: function () {
+      return this.role === "doctor";
+    },
+  },
+  code_postal: {
+    type: String,
+    required: function () {
+      return this.role === "doctor";
+    },
+  },
+
   username: String,
   email: String,
   password: String,
   role: { type: String, enum: ["patient", "doctor", "admin"] },
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
