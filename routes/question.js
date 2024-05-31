23b3f98b3d5b291584e9cd2b7f4d3448f66347d5
Dhianahdi <mongi.nahdi@gmail.com>
2024-05-31 18:35:02 +0100

    34/05

--- routes/question.js
+++ routes/question.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const questionController = require('../controllers/questionController');
 
 router.post('/', questionController.createQuestion);
+router.post('/add', questionController.addQuestion);
 router.get('/', questionController.getAllQuestions);
 router.get('/:id', questionController.getQuestionById);
 router.put('/:id', questionController.updateQuestion);
 router.delete('/:id', questionController.deleteQuestion);
+router.get('/user/:userEmail', questionController.getQuestionsByUserEmail);
+router.get('/doctor/:doctorEmail', questionController.getQuestionsByDoctorEmail);
 
 module.exports = router;
